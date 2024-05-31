956624c4150ef8b0b2314167e32e707b97881ca3
Cagatay Sen <orhancagataysen@outlook.com>
2024-05-31 19:57:48 +0200

    convert-binary-number-in-a-linked-list-to-Integer solved

---
+++ Convert-Binary-Number-in-a-Linked-List-to-Integer/Solution.java
@@ -0,0 +1,31 @@
+//  Definition for singly-linked list.
+class ListNode {
+    public int val;
+    public ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+
+
+class Solution {
+    public int getDecimalValue(ListNode head) {
+
+        if (head == null){
+            return 0;
+        }
+
+        int sum = 0;
+
+        ListNode currentNode = head;
+
+        while (currentNode != null){
+            sum = sum * 2 + currentNode.val;
+            currentNode = currentNode.next;
+        }
+        return sum;
+
+    }
+}
+
