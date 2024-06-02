61c2cb7cad13567f9fcf0ed654eea64554dbfc89
Cagatay Sen <orhancagataysen@outlook.com>
2024-06-02 15:41:47 +0200

    reverse-string solution added

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main extends Solution3{
+
+public static void main(String[] args) {
+    char[] s = {'h', 'e', 'l', 'l', 'o'};
+    System.out.println("Original array: " + String.valueOf(s));
+
+    reverseString(s);
+
+    System.out.println("Reversed array: " + String.valueOf(s));
+}
+
+}
---
+++ out/production/leetcode/README.md
@@ -0,0 +1,2 @@
+# leetcode
+I will be sharing here my leetcode solutions in Java.
