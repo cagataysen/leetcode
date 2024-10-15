60e1a026755eb23f633f163d1d985715fc884eef
Cagatay Sen <orhancagataysen@outlook.com>
2024-10-15 12:01:51 +0200

    palindrome question solved

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
-public class Main extends Solution3{
+
+
+public class Main{
 
 public static void main(String[] args) {
+
+    /*
+
     char[] s = {'h', 'e', 'l', 'l', 'o'};
     System.out.println("Original array: " + String.valueOf(s));
 
@@ -11,6 +16,13 @@ public static void main(String[] args) {
     String[] restaurants1 = {"mudurnu", "napcaz", "misirli", "abdo", "caglayan", "kebo"};
     String[] restaurants2 = {"ulus kofte", "tadim doner", "misirli", "kebo"};
     FindRestaurant.findRestaurant(restaurants1, restaurants2);
+*/
+
+
+    Palindrome palindrome = new Palindrome();
+    int x = 121;
+    System.out.println(palindrome.isPalindrome(x));
+
 
 
 }
---
+++ Palindrome-Number/Palindrome.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+
+public class Palindrome {
+    public boolean isPalindrome(int x) {
+
+        if(x < 0){
+            return false;
+        }
+
+        String stringX = String.valueOf(x);
+        StringBuilder compare = new StringBuilder();
+
+        for(int i = stringX.length() - 1; i >= 0 ; i--){
+            compare.append(stringX.charAt(i));
+        }
+
+        return stringX.equals(String.valueOf(compare));
+    }
+}
+
+
