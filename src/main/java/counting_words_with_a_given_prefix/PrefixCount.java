f1ebdda6044d2a837dbb6ff7b8c35079d20401e5
Cagatay Sen <orhancagataysen@outlook.com>
2025-03-11 10:59:15 +0100

    prefix count problem solved

---
+++ src/main/java/counting_words_with_a_given_prefix/PrefixCount.java
@@ -0,0 +1,26 @@
+package counting_words_with_a_given_prefix;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrefixCount {
+
+    public int prefixCount(String[] words, String pref) {
+
+        int count = 0;
+
+        for(String word: words){
+            if(word.contains(pref)){
+                int length = pref.length();
+                if(word.substring(0, length).equals(pref)){
+                    count++;
+                }
+
+            }
+        }
+
+        return count;
+
+    }
+
+}
